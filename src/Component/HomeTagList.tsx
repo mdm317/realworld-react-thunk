7f7bb663f1b253da861b43481f97e40242aa0b14
unknown <mdm317@naver.com>
2021-04-28 15:23:16 +0900

    fix unusedvar

--- src/Component/HomeTagList.tsx
+++ src/Component/HomeTagList.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { LocationDescriptorObject, Location } from "history";
 import queryString from "query-string";
 
